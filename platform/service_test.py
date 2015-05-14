bcce967dfd4e95b4ce3953b3286d8d8e9a44efc4
Bryan Moyles <bryan@teltech.co>
2015-05-14 10:05:04 -0400

    Separating tests based on function for the service

--- platform/service_test.py
+++ platform/service_test.py
@@ -34,11 +34,6 @@ class ServiceTestCase(unittest.TestCase):
         # Initially, no handlers should exist on a service
         self.assertEqual(self.service.handlers, {})
 
-        # Make sure the state of the connection is still the same
-        self.assertEqual(len(self.connection.channels), 1)
-        self.assertEqual(len(self.connection.channels[0].queues), 1)
-        self.assertEqual(len(self.connection.channels[0].binds), 0)
-
         handler_storage = {'requests': []}
 
         # Let's just pseudo-wrap a temporary function
@@ -63,21 +58,28 @@ class ServiceTestCase(unittest.TestCase):
         handler_func(None)
         self.assertEqual(len(handler_storage['requests']), 1)
 
-        # Now let's make sure calling the callback through the service works
+    def test_handle_callback(self):
+        # Initially, no handlers should exist on a service
+        self.assertEqual(self.service.handlers, {})
+
+        routing_key = '%d_%d' % (platform_pb2.GET, platform_pb2.DOCUMENTATION_LIST, )
+
+        handler_storage = {'requests': []}
+
         request = platform_pb2.Request(
             method      = platform_pb2.GET,
             resource    = platform_pb2.DOCUMENTATION_LIST,
             body        = 'hello'
         )
 
+        # An initial callback that has not been registered should not trigger the function
+        self.assertEqual(len(handler_storage['requests']), 0)
         self.service.handle_callback(None, MockMethod(routing_key), None, request.SerializeToString())
-        self.assertEqual(len(handler_storage['requests']), 2)
+        self.assertEqual(len(handler_storage['requests']), 0)
 
-        handled_request = handler_storage['requests'][-1]
-        self.assertEqual(request.method, handled_request.method)
-        self.assertEqual(request.resource, handled_request.resource)
-        self.assertEqual(request.body, handled_request.body)
+        # Now let's register the handler, and call it again
+        self.service.handle(platform_pb2.GET, platform_pb2.DOCUMENTATION_LIST)(lambda request: handler_storage['requests'].append(request))
 
-        # Calling the service for a routing_key that doesn't match should not invoke the handler
-        self.service.handle_callback(None, MockMethod('bad-routing-key'), None, request.SerializeToString())
-        self.assertEqual(len(handler_storage['requests']), 2) # Should remain at 2 requests
+        self.assertEqual(len(handler_storage['requests']), 0)
+        self.service.handle_callback(None, MockMethod(routing_key), None, request.SerializeToString())
+        self.assertEqual(len(handler_storage['requests']), 1)
