383ca6f1b3f70eb5c12ebd1aa073402ec9a9dfb5
Bryan Moyles <bryan@teltech.co>
2015-05-18 18:13:20 -0400

    Adding pika

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author              = 'Bryan Moyles',
     author_email        = 'bmoy117@gmail.com',
     version             = '0.0.1',
-    install_requires    = ['protobuf==2.6.1'],
+    install_requires    = ['protobuf==2.6.1', 'pika==0.9.8'],
     packages            = ['microplatform'],
     name                = 'Platform Python'
 )
