911cc9ba3a36aa38883c390c627ffc5ee8af2931
Bryan Moyles <bryan@teltech.co>
2015-05-20 11:40:06 -0400

    Renaming to microplatform_request in decorator

--- microplatform/decorators.py
+++ microplatform/decorators.py
@@ -2,8 +2,8 @@ from .platform_pb2 import Request
 
 def extract_request(f):
     def decorator(routed_message):
-        platform_request = Request().FromString(routed_message.body)
+        microplatform_request = Request().FromString(routed_message.body)
 
-        return f(routed_message, platform_request=platform_request)
+        return f(routed_message, microplatform_request=microplatform_request)
 
     return decorator
