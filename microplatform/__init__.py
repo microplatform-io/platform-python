b723ec5b44f145fd2cb3ec0aa2a8f93cc998f812
Bryan Moyles <bryan@teltech.co>
2015-05-20 16:07:22 -0400

    Adding a basic event helper for a documentation shortcut

--- microplatform/__init__.py
+++ microplatform/__init__.py
@@ -5,3 +5,11 @@ from .platform_pb2 import *
 from .publisher import AmqpPublisher
 from .service import Service, get_standard_service
 from .subscriber import AmqpSubscriber
+
+def basic_event(organization, method, resource, payload):
+    return microplatform.Event(
+        organization    = organization,
+        method          = method,
+        resource        = resource,
+        payload         = payload
+    )
